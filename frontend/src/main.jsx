a77ec8dd354c318e403dafa462a54171051e2a20
black <black313hat@gmail.com>
2026-04-26 18:47:06 +0100

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
+import { LanguageProvider } from './context/LanguageContext'
+import './index.css'
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || ''
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-    <App />
+    <LanguageProvider>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
+    </LanguageProvider>
   </GoogleOAuthProvider>
 )
